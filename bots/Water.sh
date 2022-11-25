941bbf044b1ff2039d3234ef42db33d95af520dc
NfN Orange <orange@ff4500.red>
2022-11-25 10:15:24 +0000

    Rewrote water bot to use tweet.sh

--- bots/Water.sh
+++ bots/Water.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
-RUNNINGTOTAL=0
-TEMP=$(mktemp)
-
-# Search for all the drink glasses of water tweets and extract the number of glasses
-/usr/local/bin/t search all "Drink * glasses of water" -c \
-    | cut -d"," -f4- \
-    | tail -n+2 \
-    | grep "\s[0-9]*\sglasses\s" \
-    | sed "s/.*\(\s[0-9]*\sglasses\).*/\1/" \
-    | cut -d" " -f2 >$TEMP
-
-# Add up all the glasses
-while read line; do
-	: $((RUNNINGTOTAL += line))
-done <$TEMP
-
-LINES=$(cat $TEMP | wc -l)
-
-/usr/local/bin/t update "$(echo 'Remember to drink '$((RUNNINGTOTAL / $LINES))' glasses of water a day! #PSA')"
-rm $TEMP
+# Search for all the drink glasses of water tweets and extract the number of glasses and take a mean average
+pushd $(dirname $0)
+. ../BotADay.sh
+average=$($tweet_script search -q 'Drink \* glasses of water' \
+    | jq -r .statuses[].text \
+    | sed -e "s/[^0-9. -]//g" \
+    | sed -e "s/ /\n/g" \
+    | grep -e "[0-9]" \
+    | sed -e "s/^\.//g" \
+    | datamash mean 1 \
+    | cut -d. -f1)
+$tweet_script post "Remember to drink $average glasses of water a day! #PSA"
+popd
