59939a1863fe75ba830279b8cd14862158842d93
NfN Orange <orange@ff4500.red>
2022-12-03 11:13:32 +0000

    Updated GhostTweet

--- bots/GhostTweet.sh
+++ bots/GhostTweet.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 CURRENTDIR=$(dirname $0)
-TWEET_ID=$(t update "boo!" -f "$CURRENTDIR/../data/ghost.jpg" | grep "status" | cut -d" " -f5 | sed -e"s/\`//g")
-sleep 1s
-yes | t delete status $TWEET_ID
+set -x
+pushd $CURRENTDIR
+. ../BotADay.sh
+TWEET_ID=$($tweet_script post -i "../data/ghost.jpg" "boo!" | jq -r ".id_str")
+sleep 2s
+$tweet_script delete $TWEET_ID
+popd
